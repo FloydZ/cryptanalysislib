59511deae0664d4464faa44c24f4bf126d633d05
Floyd Zweydinger <floyd.zweydinger@rub.de>
2022-11-20 00:08:35 +0100

    added gbech to shell.nix

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   name = "cryptanalysislib";
   src = ./.;
 
-  buildInputs = [ git libtool autoconf automake autogen gnumake cmake clang gtest gmp tbb libpng mpfr fplll gbenchmark];
+  buildInputs = [ git libtool autoconf automake autogen gnumake cmake clang gtest gbenchmark gmp tbb libpng mpfr fplll gbenchmark];
 
   # buildPhase = "c++ -o main main.cpp -lPocoFoundation -lboost_system";
 
