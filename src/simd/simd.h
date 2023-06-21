b87084b35717eb340df497451a51547840b6f515
Floyd <floyd.zweydinge+gitr@rub.de>
2023-06-21 17:06:12 +0200

    added simd header

--- src/simd/simd.h
+++ src/simd/simd.h
@@ -14,6 +14,14 @@ u64_2 xorb(const u64_2 a, const u64_2 b) {
 	return veorq_u64( a, b);
 }
 
+u64_2 gather(const uint64_t *ptr, const u64_2 off) {
+	return vld1q_lane_u64(ptr, off, 0);
+}
+
+void store(uint64_t *ptr, const u64_2 off) {
+	vst1q_lane_u64(ptr, off, 0);
+}
+
 void print(const u64_2 a) {
 	std::cout << a[0] << " " << a[1] << "\n";
 }
