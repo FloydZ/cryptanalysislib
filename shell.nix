25bb4c497f6e43184b6934e1e82c18a864561565
Floyd <floyd.zweydinge+gitr@rub.de>
2023-07-14 09:43:33 +0200

    updated nix-shell

--- shell.nix
+++ shell.nix
@@ -28,7 +28,7 @@ stdenv.mkDerivation {
   ] ++ (lib.optionals pkgs.stdenv.isLinux ([
 	flamegraph
 	gdb
-    # linuxKernel.packages.linux_6_0.perf
+    linuxKernel.packages.linux_6_3.perf
 	pprof
 	valgrind
 	massif-visualizer
