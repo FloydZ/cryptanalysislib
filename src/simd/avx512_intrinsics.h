c96a4e19d674b4bbf83f4f964f99e7dc67734a97
floyd <floyd.zweydinger@rub.de>
2025-10-18 14:16:51 +0200

    gitbak

--- src/simd/avx512_intrinsics.h
+++ src/simd/avx512_intrinsics.h
@@ -1539,3 +1539,50 @@ __m128i _mm_rolv_epi8(__m128i a,
 	);
 	return a;
 }
+
+#ifndef USE_AVX512IFMA
+static inline
+__m512i _mm512_mulhi_epi64(__m512i x, __m512i y) {
+    // https://stackoverflow.com/questions/28807341/simd-signed-with-unsigned-multiplication-for-64-bit-64-bit-to-128-bit
+    __m512i lo_mask = _mm512_set1_epi64(0x00000000ffffffff);
+    // Shuffle high bits with low bits in each 64-bit integer =>
+    // x0_lo, x0_hi, x1_lo, x1_hi, x2_lo, x2_hi, ...
+    __m512i x_hi = _mm512_shuffle_epi32(x, (_MM_PERM_ENUM)0xB1);
+    // y0_lo, y0_hi, y1_lo, y1_hi, y2_lo, y2_hi, ...
+    __m512i y_hi = _mm512_shuffle_epi32(y, (_MM_PERM_ENUM)0xB1);
+    __m512i z_lo_lo = _mm512_mul_epu32(x, y);        // x_lo * y_lo
+    __m512i z_lo_hi = _mm512_mul_epu32(x, y_hi);     // x_lo * y_hi
+    __m512i z_hi_lo = _mm512_mul_epu32(x_hi, y);     // x_hi * y_lo
+    __m512i z_hi_hi = _mm512_mul_epu32(x_hi, y_hi);  // x_hi * y_hi
+    
+    //                   x_hi | x_lo
+    // x                 y_hi | y_lo
+    // ------------------------------
+    //                  [x_lo * y_lo]    // z_lo_lo
+    // +           [z_lo * y_hi]         // z_lo_hi
+    // +           [x_hi * y_lo]         // z_hi_lo
+    // +    [x_hi * y_hi]                // z_hi_hi
+    //     ^-----------^ <-- only bits needed
+    //  sum_|  hi | mid | lo  |
+    
+    // Low bits of z_lo_lo are not needed
+    __m512i z_lo_lo_shift = _mm512_srli_epi64(z_lo_lo, 32);
+    
+    //                   [x_lo  *  y_lo] // z_lo_lo
+    //          + [z_lo  *  y_hi]        // z_lo_hi
+    //          ------------------------
+    //            |    sum_tmp   |
+    //            |sum_mid|sum_lo|
+    __m512i sum_tmp = _mm512_add_epi64(z_lo_hi, z_lo_lo_shift);
+    __m512i sum_lo = _mm512_and_si512(sum_tmp, lo_mask);
+    __m512i sum_mid = _mm512_srli_epi64(sum_tmp, 32);
+    //            |       |sum_lo|
+    //          + [x_hi   *  y_lo]       // z_hi_lo
+    //          ------------------
+    //            [   sum_mid2   ]
+    __m512i sum_mid2 = _mm512_add_epi64(z_hi_lo, sum_lo);
+    __m512i sum_mid2_hi = _mm512_srli_epi64(sum_mid2, 32);
+    __m512i sum_hi = _mm512_add_epi64(z_hi_hi, sum_mid);
+    return _mm512_add_epi64(sum_hi, sum_mid2_hi);
+}
+#endif
